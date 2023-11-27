bf08942698dc4c30b17f28e6d32288217b2a9064
Manraj Pannu <90409868+rnpannu@users.noreply.github.com>
2023-11-27 16:08:07 -0700

    Create citizen_management.sql

---
+++ citizen_management.sql
@@ -0,0 +1,13 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO"
+SET time_zone = "+00:00"
+
+
+CREATE TABLE 'resident'(
+    'SIN' int NOT NULL
+    'name' varchar(50)
+    'address' varchar(50)
+    'dateofbirth' varchar(8)
+    'surveillance_level' int
+)
