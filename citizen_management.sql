35dbde35b92871f683c3b9c9473fd8d778564786
Manraj Pannu <90409868+rnpannu@users.noreply.github.com>
2023-11-29 15:55:15 -0700

    Update citizen_management.sql

--- citizen_management.sql
+++ citizen_management.sql
@@ -12,11 +12,11 @@ CREATE TABLE 'resident'(
     'name' varchar(50) DEFAULT NULL,
     'address' varchar(50) DEFAULT NULL,
     'regionofresidence' varchar(20) DEFAULT NULL,
-    'dateofbirth' varchar(8) DEFAULT NULL,
-    'surveillance_level' int DEFAULT NULL,
-    'credit_score' int DEFAULT NULL,
-    PRIMARY KEY ('SIN')
-    ADD CONSTRAINT 'regionofresidence' FOREIGN KEY ('regionofresidence') REFERENCES 'region' ('name')
+    'dateofbirth' DATE DEFAULT NULL,
+    'surveillance_level' int DEFAULT 0,
+    'credit_score' int DEFAULT 0,
+    PRIMARY KEY ('SIN'),
+    
 );
 
 ---
@@ -24,11 +24,11 @@ CREATE TABLE 'resident'(
 ---
 
 INSERT INTO 'resident' ('SIN', 'name', 'address', 'regionofresidence', 'dateofbirth', 'surveillance_level', 'credit_score ') VALUES
-(123456789, 'Hasan Tareque', '4401 University Dr W, AB T1K 3M4, Lethbridge', 'Gnodgnaug', '15/06/1953', 9001, -149403384921)
-(987654321, 'Raheem Mir', '81 Nanchang Street, Xicheng District, Beijing', 'Nauhcis', '05/02/2003', 1, 999999999999)
-(786591745, 'Robert Benkoczi', '2711 US-95, Amargosa Valley, Nevada', 'Naniah', '07/10/1952', 100, 378001000000)
-(357829031, 'Nash Fischer', 'Fugging 4, 5121, Fugging', 'Gnaijehz', '30/05/2002', 4633, 4)
-(651934799, 'Kaleb Calverly', '20785 Schultes Avenue, Detroit', 'Naijuf', '04/04/2001', 8944, 0)
+(123456789, 'Hasan Tareque', '4401 University Dr W, AB T1K 3M4, Lethbridge', 'Gnodgnaug', '15/06/1953', 9001, -149403384921),
+(987654321, 'Raheem Mir', '81 Nanchang Street, Xicheng District, Beijing', 'Nauhcis', '05/02/2003', 1, 999999999999),
+(786591745, 'Robert Benkoczi', '2711 US-95, Amargosa Valley, Nevada', 'Naniah', '07/10/1952', 100, 378001000000),
+(357829031, 'Nash Fischer', 'Fugging 4, 5121, Fugging', 'Gnaijehz', '30/05/2002', 4633, 4),
+(651934799, 'Kaleb Calverly', '20785 Schultes Avenue, Detroit', 'Naijuf', '04/04/2001', 8944, 0),
 
 
 
@@ -122,28 +122,27 @@ INSERT INTO 'resident' ('SIN', 'name', 'address', 'regionofresidence', 'dateofbi
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO"
 SET time_zone = "+00:00"
 
-CREATE TABLE 'poltical party'(
-
-    'party leader' varchar(100) DEFAULT NULL
-    'party name' varchar(100) DEFAULT NULL
-    'number of the party' int DEFAULT NULL
+CREATE TABLE 'polticalParty'(
+    'leader' varchar(100) DEFAULT NULL,
+    'name' varchar(100) DEFAULT NULL,
+    'number' int DEFAULT 0,
     --'name' varchar(100) DEFAULT NULL
-    PRIMARY KEY ("party name")
+    PRIMARY KEY ("name"),
 
 );
 --------- DATA DUMPING FOR POLITICAL PARTY, AND THE ATTRIBUTES OF THEM. 
 
-INSERT INTO 'political party'('party leader', 'party name', 'party number') VALUES
-(348975987) ('Supreme Leader Nasah Euqerat', 'C756', 10)
-(457435435) ('Leader Peter Griffin', 'The Fingerlicans', 9)
-(059675194) ('Lord Darth Vader', 'The Empire', 8)
-(937528634) ('Lois Griffin', 'The Kitchen party', 7)
-(923759756) ('Bryan', 'The Yard Party of Poo', 6)
-(126348574) ('Homer Simpson', 'The Simps Party of Donuts', 5)
-(235438514) ('Bart Simpson', 'Broke Gamer Party of America', 4)
-(324536457) ('Lisa Simpson', 'Smart Party Nobody Cares' 3)
-(454689763) ('Meg', 'Shut up Meg of Feminism', 2)
-(382495769) ('Kevin Feige', 'Bad Marvel Pics of America', 1) 
+INSERT INTO 'politicalParty'('leader', 'name', 'number') VALUES
+(348975987) ('Supreme Leader Nasah Euqerat', 'C756', 10),
+(457435435) ('Leader Peter Griffin', 'The Fingerlicans', 9),
+(059675194) ('Lord Darth Vader', 'The Empire', 8),
+(937528634) ('Lois Griffin', 'The Kitchen party', 7),
+(923759756) ('Bryan', 'The Yard Party of Poo', 6),
+(126348574) ('Homer Simpson', 'The Simps Party of Donuts', 5),
+(235438514) ('Bart Simpson', 'Broke Gamer Party of America', 4),
+(324536457) ('Lisa Simpson', 'Smart Party Nobody Cares' 3),
+(454689763) ('Meg', 'Shut up Meg of Feminism', 2),
+(382495769) ('Kevin Feige', 'Bad Marvel Pics of America', 1),
 
 
 
@@ -255,15 +254,15 @@ CREATE TABLE 'region'(
     'eastBorder' int NOT NULL,
     'southBorder' int NOT NULL,
     'westBorder' int NOT NULL,
-    PRIMARY KEY ('name')
+    PRIMARY KEY ('name'),
 );
 
 INSERT INTO 'region' ('population', 'name', 'northBorder', 'eastBorder', 'southBorder', 'westBorder') VALUES
-('124840000', 'Gnodgnaug', '50', '75', '50', '75')
-('81100000', 'Nauhcis', '30', '35', '30', '35')
-('9258000', 'Naniah', '40', '60', '40', '60')
-('57370000', 'Gnaijehz', '70', '60', '70', '60')
-('38560000', 'Naijuf', '50', '45', '50', '45')
+('124840000', 'Gnodgnaug', '50', '75', '50', '75'),
+('81100000', 'Nauhcis', '30', '35', '30', '35'),
+('9258000', 'Naniah', '40', '60', '40', '60'),
+('57370000', 'Gnaijehz', '70', '60', '70', '60'),
+('38560000', 'Naijuf', '50', '45', '50', '45'),
 
 
 
@@ -276,7 +275,7 @@ INSERT INTO 'region' ('population', 'name', 'northBorder', 'eastBorder', 'southB
 
 
 
-
+ADD CONSTRAINT 'regionofresidence' FOREIGN KEY ('regionofresidence') REFERENCES 'region' ('name')
 
 
 
